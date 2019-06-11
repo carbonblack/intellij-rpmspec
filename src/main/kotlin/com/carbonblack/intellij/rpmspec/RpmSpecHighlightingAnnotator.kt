c2026dc476d2bfe0130d03975615c78269b2c337
Timothy Lusk <tlusk@carbonblack.com>
2019-06-11 02:24:12 -0400

    Fix description text color
    The text color was overflowing on top of if statements and into other
    sections.

--- src/main/kotlin/com/carbonblack/intellij/rpmspec/RpmSpecHighlightingAnnotator.kt
+++ src/main/kotlin/com/carbonblack/intellij/rpmspec/RpmSpecHighlightingAnnotator.kt
@@ -4,6 +4,7 @@ import com.carbonblack.intellij.rpmspec.psi.*
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 
 class RpmSpecHighlightingAnnotator : Annotator {
@@ -15,7 +16,9 @@ class RpmSpecHighlightingAnnotator : Annotator {
                 holder.createAnnotation(HighlightSeverity.WARNING, element.textRange, null).textAttributes = RpmSpecSyntaxHighligher.VERSION
             }
         } else if (element is RpmSpecDescriptionSection) {
-            holder.createAnnotation(HighlightSeverity.INFORMATION, element.genericSection.genericBody.textRange, null).textAttributes = RpmSpecSyntaxHighligher.TEXT
+            val startOffset = element.genericSection.genericBody.textRange.startOffset
+            val endOffset = element.genericSection.genericBody.ifExprList.firstOrNull()?.textRange?.startOffset ?: element.genericSection.textRange.endOffset
+            holder.createAnnotation(HighlightSeverity.INFORMATION, TextRange(startOffset, endOffset), null).textAttributes = RpmSpecSyntaxHighligher.TEXT
         } else if (element is RpmSpecChangelogSection) {
             holder.createAnnotation(HighlightSeverity.INFORMATION, element.genericSection.genericBody.textRange, null).textAttributes = RpmSpecSyntaxHighligher.TEXT
         } else if (element is RpmSpecTagValue) {
